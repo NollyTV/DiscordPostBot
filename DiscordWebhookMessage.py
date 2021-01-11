de6840050f223cf49ddc18a82958a06691eb361d
NollyTV <77259260+NollyTV@users.noreply.github.com>
2021-01-11 00:02:45 -0500

    Initial commit

---
+++ DiscordWebhookMessage.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+with open('.\secret.json') as f:
+    credentials = json.load(f)
+apiKey = credentials['apikey']
+data = {}
+#data["content"] = "message content"
+data["embeds"] = []
+embed = {}
+
+embed["description"] = ""
+embed["title"] = ""
+embed["color"] = "4289797"
+data["embeds"].append(embed)
+
+result = requests.post(apiKey, data=json.dumps(data), headers={"Content-Type": "application/json"})
+
+try:
+    result.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
+else:
+    print("Payload Delivered Successfully, code {}.".format(result.status_code))
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DiscordPostBot
+Simple app that creates an embedded post to a specified Discord webhook.
+## Required
+a `secret.json` file is required. Currently it utilized `apikey`. 
