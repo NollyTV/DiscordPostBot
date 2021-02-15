01445571b5c783ba200a151244f54ef204726f65
NollyTV <77259260+NollyTV@users.noreply.github.com>
2021-02-15 18:46:25 -0500

    Add Functionality to check if date=tuesday to send alt message

--- DiscordWebhookMessage.py
+++ DiscordWebhookMessage.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from datetime import date
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -14,9 +15,16 @@ data = {}
 data["embeds"] = []
 embed = {}
 
-embed["description"] = ""
-embed["title"] = ""
-embed["color"] = "4289797"
+if date.today().weekday() == 1:
+    embed["description"] = "Also remember to craft shadestones"
+    embed["title"] = "Update your lootsheet, jackalope"
+    embed["color"] = "15105570"
+    
+else:
+    embed["description"] = "Daily Reminder to transmute your shadestones"
+    embed["title"] = "Shadestone Reminder"
+    embed["color"] = "4289797"
+
 data["embeds"].append(embed)
 
 result = requests.post(apiKey, data=json.dumps(data), headers={"Content-Type": "application/json"})
