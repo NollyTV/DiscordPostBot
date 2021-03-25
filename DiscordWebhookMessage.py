a31ba00689b690e4d84513b41d77ef69eb2346a6
NollyTV <77259260+NollyTV@users.noreply.github.com>
2021-03-24 22:46:39 -0400

    Refactor script

--- DiscordWebhookMessage.py
+++ DiscordWebhookMessage.py
@@ -10,24 +10,33 @@ os.chdir(dname)
 with open('secret.json') as f:
     credentials = json.load(f)
 apiKey = credentials['apikey']
-data = {}
-#data["content"] = "message content"
-data["embeds"] = []
-embed = {}
 
 if date.today().weekday() == 1:
-    embed["description"] = "This is a threat"
-    embed["title"] = "Update your lootsheet, jackalope"
-    embed["color"] = "15105570"
-    data["embeds"].append(embed)
-    result = requests.post(apiKey, data=json.dumps(data), headers={"Content-Type": "application/json"})
+    embed = {
+        "title": "Update your lootsheet, jackalope",
+        "color": "15105570",
+        "image": {
+            "url": "https://i.imgur.com/yeEnrQU.jpeg"
+        }
+    }
+
+    data = {
+        "embeds": [
+            embed
+        ],
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    result = requests.post(apiKey, json=data, headers=headers)
     try:
         result.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err)
     else:
         print("Payload Delivered Successfully, code {}.".format(result.status_code))
-    
+
 else:
     print("It is not Tuesday, my dude")
     exit
+
